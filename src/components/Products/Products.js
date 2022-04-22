667b26f5d980f5247e8e78dcdcc00aefe2ea0a7b
vikovic18 <obiomaakobundu@gmail.com>
2022-04-22 20:37:33 +0100

    firstproductpage

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,8 +1,31 @@
-
+import products from './productsData'
+import { Row, Col, Card } from 'react-bootstrap'
 
 function Products() {
   return (
-    <div>Products</div>
+    <div className='container-fluid'>
+      <h2 className='mt-3'>MENU</h2>
+            <Row>
+              {products.map(product => (
+              <Col sm={12} md={6} lg={4} xl={3}>
+                <Card className='my-3 p-3 rounded'>
+                  <Card.Img src={product.image}/>
+                  <Card.Body>
+                    <Card.Title as='div'>
+                      <strong>{product.name}</strong>
+                    </Card.Title>
+                    <Card.Text as='h5'>
+                      N{product.price}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            )
+            )}
+            </Row>         
+        
+
+    </div>
   )
 }
 
