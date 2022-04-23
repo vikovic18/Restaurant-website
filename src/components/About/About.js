649f281e37c141f201076ee1240f232651311752
herbheebhart <abibatsalaudeen71@gmail.com>
2022-04-23 22:48:18 +0100

    firstAboutPage

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import './about.css'
 function About() {
   return (
-    <div>About</div>
+    <div className='new'>
+     <div className="main1">
+         <div className='aboutUsBtn'>
+            <h1>About Us</h1>
+            <img src="../aboutImg/veg.jpg"/>
+          </div>
+      </div>
+       <div className='aboutTest'>
+      <p>Serving up a simply straightforward (and straight-up savory) menu of Northern Italian classics like risotto, fresh pastas, crostini, antipasti, and verdure, an extensive wine list highlighting the best biodynamic and organic wines from Italy, and world class cocktails from the bar, The Tailor’s Son is a love letter to Italy woven from a different cloth.  </p>
+      </div>
+    </div>
   )
 }
 
