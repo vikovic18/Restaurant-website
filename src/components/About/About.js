b067ac3f348e5e99f940d423a4fde48ceb0b76bc
herbheebhart <abibatsalaudeen71@gmail.com>
2022-04-25 15:16:22 +0100

    add image to the about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import './about.css'
+
+// let pictures = "images/ewedu.jpg"
 function About() {
   return (
     <div className='new'>
      <div className="main1">
-         <div className='aboutUsBtn'>
-            <h1>About Us</h1>
-            <img src="../aboutImg/veg.jpg"/>
-          </div>
+          <div className='aboutBtn'>
+              <h1>About Us</h1>
+
+                <div className='aboutTest'>
+                    <p>Serving up a simply straightforward (and straight-up savory) menu of Northern Italian classics like risotto, fresh pastas, crostini, antipasti, and verdure, an extensive wine list highlighting the best biodynamic and organic wines from Italy, and world class cocktails from the bar, The Tailor’s Son is a love letter to Italy woven from a different cloth.  </p>
+                </div>
+         </div> 
       </div>
-       <div className='aboutTest'>
-      <p>Serving up a simply straightforward (and straight-up savory) menu of Northern Italian classics like risotto, fresh pastas, crostini, antipasti, and verdure, an extensive wine list highlighting the best biodynamic and organic wines from Italy, and world class cocktails from the bar, The Tailor’s Son is a love letter to Italy woven from a different cloth.  </p>
-      </div>
+       
     </div>
   )
 }
