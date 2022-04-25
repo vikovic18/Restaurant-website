0f386e4580b1c434ac7a21310b8dc52a47c518da
vikovic18 <obiomaakobundu@gmail.com>
2022-04-25 07:21:19 +0100

    'navbar

--- src/App.js
+++ src/App.js
@@ -4,29 +4,36 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import Navbar from './components/Navbar/Navbar';
+import Navbarh from './components/Navbar/Navbar';
 import HomePage from './components/HomePage/Homepage';
 import Contact from './components/Contact/Contact';
 import Products from './components/Products/Products';
-import About from  './components/About/About';
+import About from './components/About/About';
 import Footer from './components/Footer/Footer';
+import { Container } from 'react-bootstrap';
 
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navbarh />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/products" element={<Products />} />
+          </Routes>
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
-      </Routes>
-      hr
+        </Container>
+      </main>
     </div>
 
   );
 }
 
 export default App;
+
+
+
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,50 +1,38 @@
-import './navbar.css'
-import { Link } from "react-router-dom";
-import { Container, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, Row } from 'react-bootstrap';
 
 
 
 
-function Navbar() {
+
+function NavbarR() {
   return (
-   
-    <header className="">
-        <nav>
-            <div className="logo">
-                <Link to="/" className="logoStyle">WFD</Link> 
-            </div>
-            <ul className="navbar-ul">
-                <li>
-                    <Link to="/" className="headerlink-title">HOME</Link> 
-                </li>
-                <li>
-                    <Link to="/products" className="headerlink-title">PRODUCT</Link> 
-                </li>
-                <li>
-                    <Link to="/about" className="headerlink-title">ABOUT</Link> 
-                </li>
-                <li>
-                    <Link to="/contact" className="headerlink-title">CONTACT</Link> 
-                </li>
+    <div>
+      <Navbar collapseOnSelect  bg="dark" variant='dark' expand="sm">
+        <Container>
+          <Navbar.Brand href="/">BUNMI'S CUISINE</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav >
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/products">Products</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
 
-            </ul>
-        </nav>
-        {/* <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-        
-                </Nav>
-            </Navbar.Collapse>
-            </Container>
-            </Navbar> */}
-    </header>
-   
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+
+
+    </div>
+
   )
 }
 
-export default Navbar
+export default NavbarR
+
+
+
+
