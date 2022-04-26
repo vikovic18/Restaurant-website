610a14cc9c90cacb98e4390feb8687ddf7889c81
Oscar Jite-Orimiono <75793848+OscarJite@users.noreply.github.com>
2022-04-26 14:54:10 +0100

    Homepage

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,16 @@
+import './homepage.css'
+import './bootstrap.min.css'
+
+function Homepage() {
+  return (
+    <div className="container-fluid intro padding">
+      <div className="col-lg-8 col-md-8 col-sm-12">
+        <h1>SUMI <br />RESTURANT</h1>
+        <p>Welcome!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
