aea1aa50906d9bf4a37bbdfbbee32a7690a00817
vikovic18 <obiomaakobundu@gmail.com>
2022-04-26 16:32:04 +0100

    firstF

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -5,7 +5,7 @@ function Homepage() {
   return (
     <div className="container-fluid intro padding">
       <div className="col-lg-8 col-md-8 col-sm-12">
-        <h1>SUMI <br />RESTURANT</h1>
+        <h1>BUNMI'S <br />CUISINE</h1>
         <p>Welcome!</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
