5e4a0c9d16821cbcec017340e79fc21c1f28d428
vikovic18 <obiomaakobundu@gmail.com>
2022-04-27 09:41:36 +0100

    redirects

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,18 +5,18 @@ import './about.css'
 function About() {
   return (
     <div className='new' >
-     <div className="main1">
-          <div className='aboutBtn'>
-              <h1>About Us</h1>
+      <div className="main1">
+        <div className='aboutBtn'>
+          <h1>About Us</h1>
 
-                <div className='aboutTest'>
-                    <p>Serving up simple straightforward, lovely, tasty Nigerian dishes.
+          <div className='aboutTest'>
+            <p>Serving up simple straightforward, lovely, tasty Nigerian dishes.
 
-                    </p>
-                </div>
-         </div> 
+            </p>
+          </div>
+        </div>
       </div>
-       
+
     </div>
   )
 }
